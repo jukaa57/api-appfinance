26a322f7cc6270dadda4a3dcbda095b8da9387b8
Júnior O <junioroliveira.1150@gmail.com>
2022-02-03 14:47:50 -0300

    configurado

--- database/migrations/1643653050849_aluguel.ts
+++ database/migrations/1643653050849_aluguel.ts
@@ -6,11 +6,11 @@ export default class Aluguel extends BaseSchema {
   public async up () {
     this.schema.createTableIfNotExists("aluguel", (table) => {
       table.increments('id').unique()
-      table.float('Valor')
-      table.string('D_Vencimento')
-      table.string('D_Pagamento')
-      table.string('F_Pagamento')
-      table.string('Situacao')
+      table.float('valor')
+      table.string('d_vencimento')
+      table.string('d_pagamento')
+      table.string('f_pagamento')
+      table.string('situacao')
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
