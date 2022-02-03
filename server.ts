5a79ff4629f75398581f71136721d5030ce9d22e
Júnior O <junioroliveira.1150@gmail.com>
2022-02-03 13:26:41 -0300

    Ajuste reflect-metadata

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@
 |
 */
 
-import './node_modules/reflect-metadata';
+import './node_modules/reflect-metadata/index';
 import sourceMapSupport from 'source-map-support';
 import { Ignitor } from '@adonisjs/core/build/standalone';
 
