ad2ac94cf7ff8ef986e640719aafa8cdf4e5831a
Júnior O <junioroliveira.1150@gmail.com>
2022-02-03 13:43:12 -0300

    Ajuste reflect-metadata

--- server.ts
+++ server.ts
@@ -10,9 +10,9 @@
 |
 */
 
-import './node_modules/reflect-metadata/index';
-import sourceMapSupport from 'source-map-support';
-import { Ignitor } from '@adonisjs/core/build/standalone';
+import 'reflect-metadata'
+import sourceMapSupport from 'source-map-support'
+import { Ignitor } from '@adonisjs/core/build/standalone'
 
 sourceMapSupport.install({ handleUncaughtExceptions: false })
 
