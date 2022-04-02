2825e374c548e8b50ea675f4f60f31278f187c36
Júnior O <junioroliveira.1150@gmail.com>
2022-04-02 13:47:30 -0300

    comentando metacontroller

--- app/Controllers/Http/MetaController.ts
+++ app/Controllers/Http/MetaController.ts
@@ -1,53 +1,53 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-// import Database, {  } from '@ioc:Adonis/Lucid/Database';
-import Meta from 'App/Models/Meta';
+// // import Database, {  } from '@ioc:Adonis/Lucid/Database';
+// import Meta from 'App/Models/Meta';
 
-export default class MetaController {
+// export default class MetaController {
 
-    public async createMeta ({  request,  }: HttpContextContract)  {
-        const { nome_meta, valor, d_validade } = request.all();
-        const r = await Meta.create({
-            nome_meta,
-            valor,
-            d_validade
-        });
-        return r
-    }
+//     public async createMeta ({  request,  }: HttpContextContract)  {
+//         const { nome_meta, valor, d_validade } = request.all();
+//         const r = await Meta.create({
+//             nome_meta,
+//             valor,
+//             d_validade
+//         });
+//         return r
+//     }
 
-    public async deposito ({  request,  }: HttpContextContract)  {
-        const { id, valor_depositado } = request.all();
-        const s = await Meta.query().select('valor_depositado').where('id', id);
-        const r = await Meta.findOrFail(id);
-        s.map((i) =>   r.valor_depositado = i.valor_depositado + valor_depositado);
-        // r.valor_depositado =  valor_depositado,
-        await r.save();
-        return r
-    }
+//     public async deposito ({  request,  }: HttpContextContract)  {
+//         const { id, valor_depositado } = request.all();
+//         const s = await Meta.query().select('valor_depositado').where('id', id);
+//         const r = await Meta.findOrFail(id);
+//         s.map((i) =>   r.valor_depositado = i.valor_depositado + valor_depositado);
+//         // r.valor_depositado =  valor_depositado,
+//         await r.save();
+//         return r
+//     }
 
-    public async getMeta () {
-        const s = await Meta.query()
-        .select('*')
-        .orderBy('id', 'desc');
-        return s;
-    }
+//     public async getMeta () {
+//         const s = await Meta.query()
+//         .select('*')
+//         .orderBy('id', 'desc');
+//         return s;
+//     }
 
-    public async getMetaValorTotal() {
-        const s = await Meta.query().select('valor')
-        return s;
-    }
+//     public async getMetaValorTotal() {
+//         const s = await Meta.query().select('valor')
+//         return s;
+//     }
 
     
-    public async getMetaTotalDepositado({  params  }: HttpContextContract) {
-        const {id} = params.all()
-        const s = await Meta.query().select('valor_depositado').where(id)
-        return s;
-    }
+//     public async getMetaTotalDepositado({  params  }: HttpContextContract) {
+//         const {id} = params.all()
+//         const s = await Meta.query().select('valor_depositado').where(id)
+//         return s;
+//     }
 
-    public async deleteMeta ({  request,  }: HttpContextContract) {
-        const { id } = request.all();
-        const r = await Meta.findOrFail(id);
+//     public async deleteMeta ({  request,  }: HttpContextContract) {
+//         const { id } = request.all();
+//         const r = await Meta.findOrFail(id);
 
-        await r.delete();
-    }
-}  
+//         await r.delete();
+//     }
+// }  
